8aba73a18e70db3648c79d463e4851689462492f
kingabdullah <abdullahnadvi2004@gmail.com>
2026-03-01 08:17:44 -0800

    second push

--- Mad-Libs-Story-Generator.py
+++ Mad-Libs-Story-Generator.py
@@ -1,3 +1,4 @@
+# start of the program
 print()
 print("=" * 64)
 print("            WELCOME TO THE MAD LIBS STORY GENERATOR")
@@ -9,4 +10,21 @@ print("Your answers will be used to build a funny custom story.")
 print()
 print("Let's get started!")
 print()
+
+# collect user input
+noun = input("Enter a noun (person/place/thing): ")
+verb = input("Enter a verb (action word): ")
+adjective = input("Enter an adjective (describing word): ")
+adverb = input("Enter an adverb (how something is done): ")
+place = input("Enter a place: ").title()
+name = input("Enter a name: ").title()
+
+
+# generate and print story
+print()
+print("Here is your Mad Libs story:")
+print("-" * 64)
+print(f"Once upon a time, in a {adjective} {place}, there lived a {noun} named {name}.")
+print(f"Every day, {name} would {verb} {adverb}.")
+print("-" * 64)
 
